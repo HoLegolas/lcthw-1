9fae8f0b0066114f13f28ad859a5b7750a5b4b47
Jérôme Radix <jerome.radix@gmail.com>
2015-09-13 15:05:01 +0200

    added swap code to bstree.c in ex40

--- ex40/liblcthw/src/lcthw/bstree.c
+++ ex40/liblcthw/src/lcthw/bstree.c
@@ -165,3 +165,95 @@ int BSTree_traverse(BSTree *map, BSTree_traverse_cb traverse_cb)
   return 0;
 }
 
+static inline BSTreeNode *BSTree_find_min(BSTreeNode *node)
+{
+  while(node->left) {
+    node = node->left;
+  }
+
+  return node;
+}
+
+static inline void BSTree_replace_node_in_parent(BSTree *map, BSTreeNode *node, BSTreeNode *new_value)
+{
+  if(node->parent) {
+    if(node == node->parent->left) {
+      node->parent->left = new_value;
+    } else {
+      node->parent->right = new_value;
+    }
+  } else {
+    // this is the root so gotta to change it
+    map->root = new_value;
+  }
+
+  if(new_value) {
+    new_value->parent = node->parent;
+  }
+}
+
+static inline void *BSTree_swap(BSTreeNode *a, BSTreeNode *b)
+{
+  void *temp = NULL;
+  temp = b->key; b->key = a->key; a->key = temp;
+  temp = b->data; b->data = a->data; a->data = temp;
+}
+
+static inline BSTreeNode *BSTree_node_delete(BSTree *map, BSTreeNode *node, void *key)
+{
+  int cmp = map->compare(node->key, key);
+
+  if(cmp < 0) {
+    if(node->left) {
+      return BSTree_node_delete(map, node->left, key);
+    } else {
+      // not found
+      return NULL;
+    }
+  } else if(cmp > 0) {
+    if(node->right) {
+      return BSTree_node_delete(map, node->right, key);
+    } else {
+      // not found
+      return NULL;
+    }
+  } else {
+    if(node->left && node->right) {
+      // swap this node for the smallest node that is bigger than us
+      BSTreeNode *successor = BSTree_find_min(node->right);
+      BSTree_swap(successor, node);
+
+      // this leaves the old successor with possibly a right child
+      // so replace it with that right child
+      BSTree_replace_node_in_parent(map, successor, successor->right);
+
+      // finally it's swapped, so return successor instead of node
+      return successor;
+    } else if(node->left) {
+      BSTree_replace_node_in_parent(map, node, node->left);
+    } else if(node->right) {
+      BSTree_replace_node_in_parent(map, node, node->right);
+    } else {
+      BSTree_replace_node_in_parent(map, node, NULL);
+    }
+
+    return node;
+  }
+}
+
+void *BSTree_delete(BSTree *map, void *key)
+{
+  void *data = NULL;
+
+  if(map->root) {
+    BSTreeNode *node = BSTree_node_delete(map, map->root, key);
+
+    if(node) {
+      data = node->data;
+      free(node);
+    }
+  }
+
+  return data;
+}
+
