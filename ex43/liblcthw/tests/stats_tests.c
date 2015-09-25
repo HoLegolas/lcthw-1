2abd84ae7625207ab4e4cf70f6aff15caaf852dd
Jérôme Radix <jerome.radix@gmail.com>
2015-09-25 21:25:22 +0200

    added test_operations() to stats_tests.c

--- ex43/liblcthw/tests/stats_tests.c
+++ ex43/liblcthw/tests/stats_tests.c
@@ -64,3 +64,38 @@ double expect_mean = 4.8570;
 double expect_stddev = 2.606308;
 
 #define EQ(X,Y,N) (round((X) * pow(10,N)) == round((Y) * pow(10,N)))
+
+char *test_operations()
+{
+  int i = 0;
+  Stats *st = Stats_create();
+  mu_assert(st != NULL, "Failed to create stats.");
+
+  for (i = 0; i < NUM_SAMPLES; i++) {
+    Stats_sample(st, samples[i]);
+  }
+
+  Stats_dump(st);
+
+  mu_assert(EQ(st->numsq, expect.sumsq, 3), "sumsq not valid");
+  mu_assert(EQ(st->sum, expect.sum, 3), "sum not valid");
+  mu_assert(EQ(st->min, expect.min, 3), "min not valid");
+  mu_assert(EQ(st->max, expect.max, 3), "max not valid");
+  mu_assert(EQ(st->n, expect.n, 3), "n not valid");
+  mu_assert(EQ(expect_mean, Stats_mean(st), 3), "mean not valid");
+  mu_assert(EQ(expect_stddev, Stats_stddev(st), 3), "stddev not valid");
+
+  return NULL;
+}
+
+char *all_tests()
+{
+  mu_suite_start();
+
+  mu_run_test(test_operations);
+
+  return NULL;
+
+}
+
+RUN_TESTS(all_tests);
